70a8b08f856c1c6a3bbd0276886ddacff3f7c8f5
habbababbai <qba.7145@gmail.com>
2022-11-19 13:35:45 +0100

    redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,36 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { shopApi } from "../features/shopAPI/shopAPI";
 import localCartReducer from "../features/localCart/localCart";
+import { combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 // our redux store holding cart state and API
 
-export const store = configureStore({
-    reducer: {
-        [shopApi.reducerPath]: shopApi.reducer,
-        localCart: localCartReducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(shopApi.middleware),
+const persistConfig = {
+    key: "root",
+    storage: storage,
+};
+
+export const rootReducers = combineReducers({
+    [shopApi.reducerPath]: shopApi.reducer,
+    localCart: localCartReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ["persist/PERSIST"],
+            },
+        }).concat(shopApi.middleware),
+});
+
+//setupListeners(store.dispatch);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
